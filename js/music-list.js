91cd1a920257a1ce2292ecc58d048051852cda09
Pathansabiya9282 <112163376+Pathansabiya9282@users.noreply.github.com>
2023-07-19 15:09:59 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,40 @@
+let allMusic = [
+
+    {
+        name: "Jaan Nisar",
+        artist: "Arijit Singh",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Phir aur kya chahiye",
+        artist: "Arijit Singh",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "What jhumka",
+        artist: "Arijit Singh",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Chogada",
+        artist: "Darshan Raval",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "All I need",
+        artist: "Prateek",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Lost Sky - Vision NCS",
+        artist: "NCS Release",
+        img: "music-6",
+        src: "music-6"
+    },
+    
+    ];
